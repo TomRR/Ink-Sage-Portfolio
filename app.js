a31f92142d56f8f2de22d868126a098eee00460f
Tom <TomRR@github.com>
2019-04-24 23:31:49 +0200

    update app.js

--- app.js
+++ app.js
@@ -3,18 +3,10 @@ $(document).ready(function () {
     $(".sidenav").sidenav();
     // Material-boxes
     $(".materialboxed").materialbox();
-    // Parallax
-    $(".parallax").parallax();
     // Tabs
     $(".tabs").tabs();
-    // datepicker
-    $(".datepicker").datepicker({
-      disableWeekends: true
-    });
     // Tooltip
     $(".tooltipped").tooltip();
     // scrollspy - zu beachten hier: scrollSpy() mir großen S
     $(".scrollspy").scrollSpy();
-
-    $('.timepicker').timepicker();
   });
